714e0761d9e25933b175d96d1514334a9bbdf0e9
nomanabir12 <alnomanofficial@gmail.com>
2021-11-10 12:50:24 +0600

    teams app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <ChatEngine 
+    height="100vh"
+    projectID="a03a9f33-d0bb-4dde-a806-285609940975"
+    userName="abir"
+    userSecret="123123"
+      
+    />
+
+  )
+}
+
+export default App;
