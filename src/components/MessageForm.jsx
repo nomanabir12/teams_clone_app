60a8bb2e025b8b841056cb3bf3719e903bc0280d
nomanabir12 <alnomanofficial@gmail.com>
2021-11-13 12:08:20 +0600

    picture upload option

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,23 +1,43 @@
 import { useState } from "react";
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
+import { sendMessage, isTyping } from 'react-chat-engine';
 
-const MessageForm = () => {
+const MessageForm = (props) => {
     const [value, setValue] = useState('');
-    const handleSubmit = () => {
+    const { chatId, creds } = props;
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const text = value.trim();
+
+        if (text.length > 0) sendMessage(creds, chatId, { text });
+        setValue('');
 
     }
-    const handleChange = () => {
+    const handleChange = (event) => {
+        setValue(event.target.value);
+
+        isTyping(props, chatId);
+
+
 
     }
 
-    return(
+    return (
         <form className="message-form" onSubmit={handleSubmit}>
-            <input 
+            <input
                 className="message-input"
                 placeholder="Send a message..."
                 value={value}
                 onChange={handleChange}
                 onSubmit={handleSubmit}
             />
+            <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </label>
         </form>
     );
 }
