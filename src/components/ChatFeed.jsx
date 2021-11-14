4a6a3afc50b6ac621c44778e2e5c1a37a49a7f22
nomanabir12 <alnomanofficial@gmail.com>
2021-11-14 12:10:21 +0600

    chat app login page

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,22 +1,25 @@
-import MyMessage from './MyMessage';
-import TheirMessage from './TheirMessage';
-import MessageForm from './MessageForm';
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+import MessageForm from "./MessageForm";
 
 const ChatFeed = (props) => {
   const { chats, activeChat, userName, messages } = props;
 
   const chat = chats && chats[activeChat];
 
-  const renderReadReceipts = (message, isMyMessage) => chat.people.map((person, index) => person.last_read === message.id && (
-    <div
-      key={`read_${index}`}
-      className="read-receipt"
-      style={{
-        float: isMyMessage ? 'right' : 'left',
-        backgroundImage: person.person.avatar && `url(${person.person.avatar})`,
-      }}
-    />
-  ));
+  const renderReadReceipts = (message, isMyMessage) =>
+    chat.people.map((person, index) => person.last_read === message.id && (
+          <div
+            key={`read_${index}`}
+            className="read-receipt"
+            style={{
+              float: isMyMessage ? "right" : "left",
+              backgroundImage:
+                person.person.avatar && `url(${person?.person?.avatar})`,
+            }}
+          />
+        )
+    );
 
   const renderMessages = () => {
     const keys = Object.keys(messages);
@@ -27,13 +30,17 @@ const ChatFeed = (props) => {
       const isMyMessage = userName === message.sender.username;
 
       return (
-        <div key={`msg_${index}`} style={{ width: '100%' }}>
+        <div key={`msg_${index}`} style={{ width: "100%" }}>
           <div className="message-block">
-            {isMyMessage
-              ? <MyMessage message={message} />
-              : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />}
+            {isMyMessage 
+            ? <MyMessage message={message} />
+             : <TheirMessage message={message} lastMessage={messages[lastMessageKey]}/>
+            }
           </div>
-          <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+          <div
+            className="read-receipts" style={{ marginRight: isMyMessage ? "18px" : "0px", marginLeft: isMyMessage ? "0px" : "68px",
+            }}
+            >
             {renderReadReceipts(message, isMyMessage)}
           </div>
         </div>
@@ -52,7 +59,7 @@ const ChatFeed = (props) => {
         </div>
       </div>
       {renderMessages()}
-      <div style={{ height: '100px' }} />
+      <div style={{ height: "100px" }} />
       <div className="message-form-container">
         <MessageForm {...props} chatId={activeChat} />
       </div>
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const authObject = { 'Project-ID':"a03a9f33-d0bb-4dde-a806-285609940975", 'User-Name': username, 'User-Secret': password };
+
+        try {
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+
+            window.location.reload();
+            setError('');
+
+        } catch (error) {
+
+        }
+    }
+
+
+    return (
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Start chatting</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+
+
+}
+
+export default LoginForm;
